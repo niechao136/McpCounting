389c4cf1670ece45fd8af0647317537ac05639b9
nie.chao <nie.chao@advantech.com.cn>
2025-07-29 17:59:54 +0800

    dotnet 版本改为 8.0

---
+++ McpCounting/Tools/DataTool.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+
+using McpCounting.Resources;
+
+using ModelContextProtocol.Server;
+
+namespace McpCounting.Tools;
+
+[McpServerToolType]
+public class DataTool(IHttpClientFactory httpClientFactory)
+{
+    private readonly HttpClient _client = httpClientFactory.CreateClient("CustomClient");
+
+    [McpServerTool, Description("根据 api 地址、token、门店信息、时间范围、时间单位获取门店的客流信息，包括来店人数、店外人数和进店率")]
+    public async Task<string> GetWidget(
+        [Description("api 地址")] string api,
+        [Description("token")] string token,
+        [Description("需要获取客流数据的门店信息")] List<Store> stores,
+        [Description("客流数据的时间范围，包括开始日期和结束日期，格式为YYYY/MM/DD")] List<string> date,
+        [Description("客流数据的时间单位，包括小时-hh，日-dd，周-ww，月-mm，年-yyyy")] string unit
+    )
+    {
+        return "";
+    }
+}
---
+++ McpCounting/Tools/DateTool.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+
+using ModelContextProtocol.Server;
+
+namespace McpCounting.Tools;
+
+[McpServerToolType]
+public class DateTool
+{
+    [McpServerTool, Description("根据指定时区（格式为 ±HH:mm）获取当前日期，常用于没有具体日期时以本地时间为参考")]
+    public async Task<string> GetToday([Description("时区偏移量，格式为 ±HH:mm，如 +08:00 或 -05:00")] string offset)
+    {
+        if (!TimeSpan.TryParse(offset, out TimeSpan timeZoneOffset))
+        {
+            throw new ArgumentException("无效的时区格式，应为 ±HH:mm，例如 +08:00");
+        }
+
+        DateTime utcNow = DateTime.UtcNow;
+        DateTime localTime = utcNow + timeZoneOffset;
+
+        var today = localTime.Date.ToString("yyyy-MM-dd");
+        return await Task.FromResult(today);
+    }
+}
